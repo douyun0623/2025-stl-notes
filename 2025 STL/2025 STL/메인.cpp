b71b22a774a40f01b6b909cab42d2e4348104dfa
asd <asd@Adew>
2025-06-17 15:10:10 +0900

    15주 1일

--- 2025 STL/2025 STL/메인.cpp
+++ 2025 STL/2025 STL/메인.cpp
@@ -1,47 +1,34 @@
 //---------------------------------------------------------------------------
-// 2025 STL 화56목78	 6월 12일 목요일							(14주 2일)
+// 2025 STL 화56목78	 6월 17일 화요일								(15주 1일)
 // 6월 19일 목요일 15주 2일 - 기말시험, E320에서...
+//---------------------------------------------------------------------------
+// concepts - 템플릿 함수의 인자는 자료형(아닌 경우도 있다 - non-type template 
+// parameter)인데 이 자료형의 문법적 그리고 의미에서도 작성자가 의도한 것인지를
+// compile-tyime에 판단하기 위한 C++언어의 기능
+//
+// ranges와 concepts을 사용하여 STL의 라이브러리를 재작성한 것이
+// constrained algorithm이다.
+// 이 알고리즘들은 ranges::algorith에 있다.
 //---------------------------------------------------------------------------
 
 #include <iostream>
-#include <vector>
-#include <list>	
 #include <concepts>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <ranges>
 #include "save.h"
 #include "STRING.h"
 using namespace std;
 
 extern bool 관찰;					// 관찰하려면 true로 설정
 
-template <class Iter>
-iterator_traits<Iter>::difference_type my_distance(Iter b, Iter e) 
-{
-	// 만약 Iter가 random_access_iterator_tag 라면
-	// c++20의 concept을 이용하면 쉽다.
-
-	//  C++17 이후에는 if-constexpt
-	if constexpr (random_access_iterator<Iter>)	// Iter가 랜덤 access야?
-		return e - b;
-	else {
-		typename iterator_traits<Iter>::difference_type d{};
-		while (b != e) {
-			++b;
-			++d;
-		}
-		return d;
-	}
-}
-
 //--------
 int main()
 //--------
 {
-	// 반복자 간의 거리를 재는 distance
-	vector<int> v{ 1,2,3,4,5 };
-	list<int> c{ 1,2,3,4,5 };
-
-	cout << "vector의 distance - " << my_distance(v.end(), v.begin()) << endl;
-	cout << "list의 distance -   " << my_distance(c.end(), c.begin()) << endl;
+	vector<int> v{ 1,2,3 };
+	list<int> c{ 1,2,3 };
 
 	save("메인.cpp");
 } 
